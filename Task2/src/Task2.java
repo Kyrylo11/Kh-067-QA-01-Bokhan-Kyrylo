3f66a105538f4971eab32b97bed1ab3391f34da7
Kyrylo Bokhan <apllstar11@gmail.com>
2022-05-13 09:57:26 +0300

    Old version of Task2

--- Task2/src/Task2.java
+++ Task2/src/Task2.java
@@ -2,34 +2,11 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
+        System.out.println("What is your name?");
         Scanner sc = new Scanner(System.in);
-        double n1 = 0, n2 = 0, res = 0;
-        char operation = 0;
-        System.out.println("Input the first number:");
-        n1 = sc.nextDouble();
-        System.out.println("Input the operation(+, -, *, /, %):");
-        operation = sc.next().charAt(0);
-        System.out.println("Input the second number:");
-        n2 = sc.nextDouble();
-        if (operation == '+') {
-            res = n1 + n2;
-        } else if (operation == '-') {
-            res = n1 - n2;
-        } else if (operation == '*') {
-            res = n1 * n2;
-        } else if (operation == '/') {
-            if (n2 == 0) {
-                System.out.println("Can't divide by 0");
-                System.exit(1);
-            } else {
-                res = n1 / n2;
-            }
-        } else if (operation == '%') {
-            res = n1 % n2;
-        } else {
-            System.out.println("Error. Unidentified operation.");
-            System.exit(1);
-        }
-        System.out.println("Result: " + res);
+        String name = sc.nextLine();
+        System.out.println(String.format("Where are you live, %s?", name));
+        String address = sc.nextLine();
+        System.out.println(String.format("%s live now in %s", name, address));
     }
 }
